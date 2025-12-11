359a45c0bce57888a4cba94a58f5b3e6fd19cb4f
hindbio <hindhach93@gmail.com>
2025-12-11 21:06:56 +0100

    Implement categorization of GC percentage
    Added a new column 'Catégorie GC' based on GC percentage.

--- marwa.py
+++ marwa.py
@@ -40,3 +40,16 @@ print(df_filtered)
 moyenne_gc = df["Pourcentage_GC"].mean()
 print("\nPourcentage moyen de GC :", round(moyenne_gc, 3))  
 
+
+# 5) Ajouter colonne "Catégorie GC"
+def categ(gc):
+    if gc > 55:
+        return "Riche"
+    elif 45 <= gc <= 55:
+        return "Moyen"
+    else:
+        return "Faible"
+
+df["Catégorie_GC"] = df["Pourcentage_GC"].apply(categ)
+print(df)
+
